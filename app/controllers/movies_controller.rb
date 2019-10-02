6d90574dcef2bae7c2a5aeedf58c8c0e07fc94f8
OLIVER KAO <KAO.OLIVER@TAMU.EDU>
2019-10-01 22:22:44 -0500

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -17,6 +17,16 @@ class MoviesController < ApplicationController
 
     if params[:ratings]
       @ratings = params[:ratings].keys
+      session[:filtered_rating] = @ratings
+    elsif session[:filtered_rating]
+      query = Hash.new
+      session[:filtered_rating].each do |rating|
+        query['ratings['+ rating + ']'] = 1
+      end
+      query['sort'] = params[:sort] if params[:sort]
+      session[:filtered_rating] = nil
+      flash.keep
+      redirect_to movies_path(query)
     else
       @ratings = @all_ratings
     end
